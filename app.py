97cb39594032bac721bc2ae806cfbd608f9f34d5
doleebest <sj27419@gmail.com>
2024-10-21 20:06:25 +0900

    fix : app.py render template

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import sys
 
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/") # /를 만날 때 마다 hello()를 실행
 def hello() :
-    return "Hello my webpage!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0',debug=True)
