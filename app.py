665b7258244874dc3853786bd26d5a85f633d5ff
doleebest <sj27419@gmail.com>
2024-10-21 23:01:07 +0900

    fix : post 요청 터미널에 찍히도록

--- app.py
+++ app.py
@@ -38,10 +38,12 @@ def reg_item_submit():
 
 @application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post():
+    
     image_file=request.files["file"]
-    image_file.save("/static/images/{}".format(image_file.filename))
+    image_file.save("static/images/{}".format(image_file.filename))
     
     data=request.form
+    print(data)  # POST 요청에서 form 데이터를 출력
     return render_template("submit_item_result.html", data=data,
 img_path="static/images/{}".format(image_file.filename))
     
