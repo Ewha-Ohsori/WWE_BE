d19ae3914abdbc1629c35a21e6cc390c213a24f0
doleebest <sj27419@gmail.com>
2024-10-21 20:19:43 +0900

    feat : add application routes on app.py

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ application = Flask(__name__)
 def hello() :
     return render_template("index.html")
 
-if __name__ == "__main__":
-    application.run(host='0.0.0.0',debug=True)
+# if __name__ == "__main__":
+#    application.run(host='0.0.0.0',debug=True)
+
+@application.route("/list")
+def view_list():
+    return render_template("list.html")
+
+@application.route("/review")
+def view_review():
+    return render_template("review.html")
+
+@application.route("/reg_items")
+def reg_item():
+    return render_template("reg_items.html")
+
+@application.route("/reg_reviews")
+def reg_review():
+    return render_template("reg_reviews.html")      
