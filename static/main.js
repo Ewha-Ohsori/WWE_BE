7b664bfe99327723553dc8cee21f209a714438b2
doleebest <sj27419@gmail.com>
2024-10-23 20:30:57 +0900

    최종

--- static/main.js
+++ static/main.js
@@ -1,1 +1,9 @@
 // index.html decoration
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleBtn = document.querySelector('.navbar__toogleBtn');
+    const menu = document.querySelector('.navbar__menu');
+
+    toggleBtn.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
